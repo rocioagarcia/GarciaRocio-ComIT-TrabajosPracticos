b9427c3259d77f59f40e71425a2e648c4eb25b4d
rocio <rocioagarcia94@gmail.com>
2021-05-23 19:20:30 -0300

    Entrega del Ejercicio 4 del Trabajo Practico 5.

--- TP5_Ejercicio4/src/Main.java
+++ TP5_Ejercicio4/src/Main.java
@@ -19,17 +19,11 @@ public static void main(String[] args) {
 	
 	private static void datosExamenFinal (String[] alumnos, int[] notas) {
 		Random rand = new Random();
-		
 		int i;
-		int nroAlumno = 0;
 		
 		for (i = 0 ; i < alumnos.length ; i++) {
-			nroAlumno ++ ;
-			String apellido = "Alumno/a " + nroAlumno;
-			alumnos[i] = apellido;
-			
-			int nota = rand.nextInt(10) + 1;
-			notas[i] = nota;
+			alumnos[i] = "Alumno/a " + (i + 1);
+			notas[i] = rand.nextInt(10) + 1;
 			
 //			Para ver todos los apellidos y notas descomentar lo siguiente:
 //			System.out.println(alumnos[i] + "\t" + notas[i]);
@@ -38,15 +32,12 @@ public static void main(String[] args) {
 	
 	private static int promedioNotas (int[] notas) {
 		int totalNotas = 0;
-		int promedio;
 
 		for (int i = 0 ; i < notas.length ; i++){
 		totalNotas += notas[i];
 		}
 
-		promedio = totalNotas / notas.length;
-
-		return promedio;
+		return totalNotas / notas.length;
 		
 	}
 
